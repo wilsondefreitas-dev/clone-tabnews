0c0e189d155002d3befe8beebcef2ce9cf5ace2e
Wilson de Freitas Jr <wilsondefreitas92@gmail.com>
2023-08-08 23:32:31 +0000

    add reference on the quote

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-function Home(){
+function Home() {
     return (
-        <h1>Irmão, você é o único representante do seu sonho na face da terra. Se isso não fizer você se mexer, meu chapa, eu não sei o que vai...</h1>
+        <>
+            <h1>
+                <q>Irmão, você é o único representante do seu sonho na face da terra. Se isso não fizer você se mexer, meu chapa, eu não sei o que vai...</q>
+            </h1>
+            <p>- Emicida</p>
+        </>
     )
 };
 
