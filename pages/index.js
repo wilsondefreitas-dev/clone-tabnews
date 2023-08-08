7db21d1428adabf334ae670dc75c17379d2be15d
Wilson de Freitas Jr <wilsondefreitas92@gmail.com>
2023-08-08 23:05:27 +0000

    create index route with a simple component

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return (
+        <h1>Irmão, você é o único representante do seu sonho na face da terra. Se isso não fizer você se mexer, meu chapa, eu não sei o que vai...</h1>
+    )
+};
+
+export default Home;
